bf0297074c1733a85203f71e49d27bbcc8f13992
christopher-alden <christopheralden04@gmail.com>
2024-04-29 17:02:13 +0700

    providernya ketinggalan

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { router } from '@lib/routes/route';
 import './App.css';
 import {  RouterProvider} from "react-router-dom";
+import AuthContextProvider from './context/auth-context';
 
 function App() {
 
   return (
     <div >
       {/* nanti ini context aja masukingnya */}
-      <RouterProvider router={router} />
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
     </div>
   );
 }
